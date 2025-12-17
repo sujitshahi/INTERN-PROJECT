fa4ca9f5054717bdd0999e0e2a96dbca7719886a
sujitshahi <shahisujit9@gmail.com>
2025-12-17 20:43:00 +0545

    changed

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import HomePage from "./component/HomePage";
+
+export default function Page() {
+  return (
+    <div>
+    <HomePage />
+      
+    </div>
+  )
+}
