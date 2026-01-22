fa89532f8995417c17e32b6d8fed08499e57d13a
sujitshahi <shahisujit9@gmail.com>
2026-01-22 19:25:42 +0545

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import HomePage from "./component/homepage/page";
 import School_Facilities from "./school_facilities/page";
-import Extra from "./component/extra";
 import Teacher from "./teacher/page";
 import Classes from "./classes/page";
 import Appointment from "./appointment/page";
 import Popular_Teachers from "./popular_teacher/page";
 import Testimonial from "./testimonial/page";
+import Extra from "./component/extra";
 
 export default function Page() {
   return (
