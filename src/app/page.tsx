692cf84ca66ab5c3f5f932bb413c4ccf9a3c9edf
sujitshahi <shahisujit9@gmail.com>
2026-01-22 21:33:48 +0545

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomePage from "./component/HomePage/page";
 import School_Facilities from "./School_Facilities/page";
 import Teacher from "./Teacher/page";
-import Classes from "./Classes/page";
+// import Classes from "./Classes/page";
 import Appointment from "./Appointment/page";
 import Popular_Teachers from "./Popular_Teacher/page";
 import Testimonial from "./Testimonial/page";
 import Extra from "./component/Extra";
+// import classes from "./Classes/page";
+import Classes from "./classes/page";
 
 export default function Page() {
   return (
@@ -14,10 +16,11 @@ export default function Page() {
     <School_Facilities />
     <Extra />
     <Teacher />
-    <Classes />
+    {/* <Classes /> */}
     <Appointment />
     <Popular_Teachers />
     <Testimonial />
+    <Classes /> 
     </div>
   )
 }
