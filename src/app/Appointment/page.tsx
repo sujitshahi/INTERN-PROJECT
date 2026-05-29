ac8a1faeebfccbef3e46d7340845c6eef1aec575
sujitshahi <shahisujit9@gmail.com>
2026-05-29 16:54:31 +0545

    changes

--- src/app/Appointment/page.tsx
+++ src/app/Appointment/page.tsx
@@ -10,7 +10,7 @@ export default function page() {
           className="flex-1 bg-[#FFF5F3] p-4"
           initial={{ opacity: 0, y: 80 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
+          transition={{ duration: 2, ease: "easeOut" }}
           viewport={{ once: true, amount: 0.2 }}
         >
           <h2 className=" text-3xl font-bold mb-8">
